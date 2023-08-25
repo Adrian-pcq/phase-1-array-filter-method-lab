8069366f8bb0f6e7f699f392509384cb256fbf09
Adrian <adrianqueizan@gmail.com>
2023-08-25 11:22:01 -0400

    done filter-array lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(value,string){
+  return value.filter(word => word.toLowerCase()
+  ===string.toLowerCase())
+}
+
+function fuzzyMatch(array,string){
+return array.filter(word =>  word.substring(0,2) === string.substring(0,2) )
+}
+
+function matchName(array,string){
+  const result = array.filter(word => word.name === string)
+  return result
+}
+
+
